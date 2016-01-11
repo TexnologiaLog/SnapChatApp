a7bc0b4b1f58ad80cc61e8e43dd8cca5435f288c
georkost4 <georkost4@teiser.gr>
2016-01-11 02:10:50 +0200

    update

--- app/src/main/java/snapchattapp/texnlog/com/snapchatapp/UserConnection/ServerRequests.java
+++ app/src/main/java/snapchattapp/texnlog/com/snapchatapp/UserConnection/ServerRequests.java
@@ -28,7 +28,7 @@ import java.util.ArrayList;
 public class ServerRequests {
     ProgressDialog progressDialog;
     public static final int CONNECTION_TIMEOUT = 1000 * 15;
-    public static final String SERVER_ADDRESS = "http://projectdb.esy.es";
+    public static final String SERVER_ADDRESS = "http://projectdb.esy.es/";
 
     public ServerRequests(Context context) {
         progressDialog = new ProgressDialog(context);
